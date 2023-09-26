d01002e1603f83ebf9ecdfab2bb41a4c7e782353
Gyan <pgyan4224@gmail.com>
2023-09-26 05:51:45 +0530

    Added html css and js
    html and css was fine but couldn't find out why js couldn't read the clicks

---
+++ script.js
@@ -0,0 +1,34 @@
+let counterNumber = document.getElementById("counterValue");
+
+function onDec(){
+let preValue = counterNumber.textContent;
+let currentValue = parseInt(preValue) - 1;
+if(currentValue > 0){
+    counterNumber.style.color="green"
+}else if(currentValue<0){
+    counterNumber.style.color ="red"
+}else{
+    counterNumber.style.color ="black";
+}
+counterNumber.textContent = currentValue;
+}
+
+function onInc(){
+    let preValue = counterNumber.textContent;
+    let currentValue = parseInt(preValue) + 1;
+    if(currentValue > 0){
+        counterNumber.style.color="green"
+    }else if(currentValue<0){
+        counterNumber.style.color ="red"
+    }else{
+        counterNumber.style.color ="black";
+    }
+    counterNumber.textContent = currentValue;
+    }
+
+
+function onReset(){
+    counterNumber = 0;
+    counterNumber.style.color = "black";
+    document.getElementById("Bg-Cont").style.backgroundColor = "red";
+}
